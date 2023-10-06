dde4ef5dbde91132d965fc55b9dbf0eb8e9ed1b5
roxanamanolea <mihai.roxana90@yahoo.com>
2023-10-06 13:23:34 +0100

    first commit

---
+++ src/components/contactForm/ContactForm.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export const ContactForm = ({
+  name,
+  setName,
+  phone,
+  setPhone,
+  email,
+  setEmail,
+  handleSubmit
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type='text'
+          name='name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder='Contact Name'
+          araia-label='Contact Name'
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type='tel'
+          name='phone'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          pattern='^07([\d]{3})[(\D\s)]?[\d]{3}[(\D\s)]?[\d]{3}$'
+          placeholder='Contact Phone (###-###-####)'
+          aria-label='Contact Phone'
+        />
+      </label>
+      <br />
+      <label>
+        <input 
+          tyype='email'
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder='Contact Email'
+          aria-label='Contact Email'
+        />
+      </label>
+      <br />
+      <input type='submit' value='Add Contact' aria-label='Add Contact' />
+    </form>
+  );
+};
+
